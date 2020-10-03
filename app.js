d1e82e5b0e63758a82541105e333e6570338cfc4
Isa Conteh <iconteh21@gmail.com>
2020-10-03 16:38:03 -0400

    got started with integration tests for routes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const todoRoutes = require("./routes/todo.routes");
+
+app.use(express.json());
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
     res.json("Hello World!");
 });
 
-app.listen(3000, () => {
-    console.log("Server is now running");
-})
+// app.listen(3000, () => {
+//     console.log("Server is now running");
+// })
+
+module.exports = app;
