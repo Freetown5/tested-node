ee6c7ef205a98fa7a34698351f66c44383ce890a
Isa Conteh <iconteh21@gmail.com>
2020-10-03 16:56:56 -0400

    added mongoose connection

--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -1,1 +1,5 @@
-// mongodb+srv://I_Con:<password>@cluster0.lfwrb.mongodb.net/<dbname>?retryWrites=true&w=majority
+const mongoose = require("mongose");
+
+async function connect(){
+    await mongoose.connect(`mongodb+srv://${process.env.API_USERNAME}:${process.env.API_KEY}@cluster0.lfwrb.mongodb.net/${process.env.APINAME}?retryWrites=true&w=majority`)
+}
