c39df427048c8ae2a2f55992bcb9bcfc640b5689
Isa Conteh <iconteh21@gmail.com>
2020-10-02 20:51:26 -0400

    added model to unit test and contoller

--- test/unit/todo.contoller.test.js
+++ test/unit/todo.contoller.test.js
@@ -1,7 +1,16 @@
 const TodoController = require("../../controller/todo.controller");
+const TodoModel = require("../../model/todo.model");
+
+// overrides the model function and simply calls it
+TodoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
     it("todo function should exist", () => {
         expect(typeof TodoController.createTodo).toBe("function");
     });
+
+    it("should call the TodoModel.create", () => {
+        TodoController.createTodo();
+        expect(TodoModel.create).toBeCalled()
+    })
 });
