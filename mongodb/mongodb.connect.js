1e69cedc4cf1e0dfd84493fb9e4a0c892c11e01e
Isa Conteh <iconteh21@gmail.com>
2020-10-04 20:28:16 -0400

    got rid of url parser warning

--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -5,7 +5,7 @@ async function connect(){
     try{
         await mongoose.connect(
             `mongodb+srv://${process.env.API_USERNAME}:${process.env.API_KEY}@cluster0.lfwrb.mongodb.net/${process.env.API_APPNAME}?retryWrites=true&w=majority`,
-            { useNewUrlParser: true }
+            { useNewUrlParser: true, useUnifiedTopology: true }
         );
     } catch (err) {
         console.error("Error connecting to mongodb");
