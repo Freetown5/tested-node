5d68d2a505782e98e0829fe3467b20166f88fbe8
Isa Conteh <iconteh21@gmail.com>
2020-10-05 13:29:23 -0400

    added status code for errors to app and formatted message

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const mongodb = require("./mongodb/mongodb.connect");
 mongodb.connect({ useUnifiedTopology: true });
 
 app.use(express.json());
+
 app.use("/todos", todoRoutes);
+
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message });
+});
 
 app.get("/", (req, res) => {
     res.json("Hello World!");
