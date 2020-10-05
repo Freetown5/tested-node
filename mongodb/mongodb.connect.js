330e553d983afef65c8d2764c156200f5220e59e
Isa Conteh <iconteh21@gmail.com>
2020-10-04 20:16:33 -0400

    added dotenv config, mongoose now working

--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 async function connect(){
-    console.log(process.env.API_USERNAME);
     try{
         await mongoose.connect(
-            `mongodb+srv://${process.env.API_USERNAME}:${process.env.API_KEY}@cluster0.lfwrb.mongodb.net/${process.env.APINAME}?retryWrites=true&w=majority`,
+            `mongodb+srv://${process.env.API_USERNAME}:${process.env.API_KEY}@cluster0.lfwrb.mongodb.net/${process.env.API_APPNAME}?retryWrites=true&w=majority`,
             { useNewUrlParser: true }
         );
     } catch (err) {
