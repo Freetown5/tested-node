7a72c385700429bb303696989f886679ed7b42bc
Isa Conteh <iconteh21@gmail.com>
2020-10-10 20:10:21 -0400

    added error handling for missing values

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -42,7 +42,12 @@ exports.updateTodo = async (req, res, next) => {
                 useFindAndModify: false
             }
         );
-        res.status(200).json(updatedTodo);
+        if(updatedTodo){
+            res.status(200).json(updatedTodo);
+        }else{
+            res.status(404).send();
+        }
+        
     }catch (err){
         next(err);
     }
